c9832eacb56d741b3f607ec1121598ba1af37454
d-shcherbatyy <dmitry.shcherbatyy@gmail.com>
2021-04-13 18:36:49 +0300

    Multithreading stuff

--- DeepCsharp.Multithreading/Program.cs
+++ DeepCsharp.Multithreading/Program.cs
@@ -6,7 +6,7 @@ namespace DeepCsharp.Multithreading
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      Console.WriteLine("Hello Multithreading!");
     }
   }
 }
