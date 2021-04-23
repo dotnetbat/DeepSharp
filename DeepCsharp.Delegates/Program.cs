d1cf10edf0c5e9cd289f6e724a4f83e0b8579d3f
d-shcherbatyy <dmitry.shcherbatyy@gmail.com>
2021-04-23 19:52:18 +0300

    Zamykanija

--- DeepCsharp.Delegates/Program.cs
+++ DeepCsharp.Delegates/Program.cs
@@ -11,6 +11,26 @@ namespace DeepCsharp.Delegates
       DelegatesEventsClient.DoClientWork();
       
       CoContravarianceClient.DoClientWork();
+      
+      int[] array = new int[10];
+
+      Func<int>[] arrayOfDataProviders = new Func<int>[10];
+
+      for (int i = 0; i < 10; i++)
+      {
+        arrayOfDataProviders[i] = () => i;
+      }
+
+      int j = 0;
+      foreach (var dataProvider in arrayOfDataProviders)
+      {
+        array[j++] = dataProvider();
+      }
+
+      for (int i = 0; i < 10; i++)
+      {
+        Console.WriteLine(array[i]);
+      }
     }
   }
 }
